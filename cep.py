65e1ae35bafbf51ec3214ce66718b7212c7758d1
João Barabba <jvbarabba@gmail.com>
2022-03-31 08:49:28 -0300

    Aplicação completa

--- cep.py
+++ cep.py
@@ -1,16 +1,14 @@
 import requests
-import os
 
-os.system('clear')#Limpando o terminal
 
 cep = input("Digite o CEP: ")
-r = requests.get("viacep.com.br/ws/{}/json/".format(cep))
+r = requests.get("http://viacep.com.br/ws/{}/json/".format(cep))
 
-os.system('clear')
-
-data = r.json()
+data= r.json()
 
 print('CEP: {}'.format(data['cep']))
 print('Bairro: {}'.format(data['bairro']))
-print('Cidade: {}-{}'.format(data['localidade','uf']))
-      
+print('Cidade: {}'.format(data['localidade']))
+print('Cidade: {}'.format(data['uf']))
+
+exit()
